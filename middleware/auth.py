a2b4e1228e3c92a6499c430ede5368dc26f09597
injet-zhou <zhouyingjie@sinkcode.cn>
2024-04-29 15:17:45 +0800

    fix: auth for all route

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,7 +1,7 @@
 from fastapi.responses import JSONResponse
-from rich import print
 from starlette.types import ASGIApp
 from starlette.middleware.base import BaseHTTPMiddleware
+
 
 class AuthMiddleware(BaseHTTPMiddleware):
     def __init__(self, app: ASGIApp) -> None:
@@ -9,10 +9,19 @@ class AuthMiddleware(BaseHTTPMiddleware):
 
     async def dispatch_func(self, request, call_next):
         authorization = request.headers.get("Authorization")
-        if not authorization:
-            return JSONResponse(status_code=401, content={"detail": "Authorization header required"})
-        if not authorization.startswith("Bearer "):
-            return JSONResponse(status_code=401, content={"detail": "Bearer token required"})
+        path = request.url.path
+        if path.endswith("/chat/completions"):
+            if not authorization:
+                return JSONResponse(
+                    status_code=401, content={"detail": "Authorization header required"}
+                )
+            if not authorization.startswith("Bearer "):
+                return JSONResponse(
+                    status_code=401, content={"detail": "Bearer token required"}
+                )
+            if not authorization.strip("Bearer "):
+                return JSONResponse(
+                    status_code=401, content={"detail": "Bearer token required"}
+                )
         response = await call_next(request)
         return response
-
